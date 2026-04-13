be6a7ce28831b86327cb161c3a8bbb07d9ac2b9f
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-13 03:15:31 -0400

    Create RawCod.js

---
+++ RawCod.js
@@ -0,0 +1,8 @@
+// RawCod.js - raw JavaScript code for execution or export
+console.log("Raw Code Weaver JS");
+function executeRaw() {
+    return "Raw JS executed";
+}
+const sampleCode = `// Paste raw code here
+alert("Hello from RawCod.js");
+`;
